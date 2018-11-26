d07cef8df0ae0f400841f574bebbfb39e69931a5
larabellatin <40076607+larabellatin@users.noreply.github.com>
2018-11-26 15:12:39 -0500

    Update main.cpp

--- Version 1/main.cpp
+++ Version 1/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 #include <string>
-#include "Definiciones.h"
 #include "Ficha.h"
 #include "Jugador.h"
+#include "Computadora.h"
+#include "Persona.h"
+#include <random>
+#include <time.h>
 
 using namespace std;
+extern TipoCadena *Ludo;
 
 
 int main() {
+
+    TipoEntero mayorPosicion=15;
+
+    srand(time(nullptr));
+    Ludo=new TipoCadena[104];
+
+    CrearTablero(Ludo);
+
     TipoEntero numero;
     TipoCadena id;
-    Jugador *pJ1, *pJ2, *pJ3, *pJ4;
+    Jugador *pJ1=nullptr;
+    Jugador *pJ2=nullptr;
+    Jugador *pJ3=nullptr;
+    Jugador *pJ4=nullptr;
 
     do {
         cout << "Escoge la cantidad de jugadores (1-4): ";
@@ -20,46 +35,49 @@ int main() {
     switch(numero){
         case 1:
             cout << "Jugador 1 ingrese su identificador: "; cin >> id;
-            pJ1 = new Jugador(true, id, 1);
-            pJ2 = new Jugador(false, "COM1", 2);
-            pJ3 = new Jugador(false, "COM2", 3);
-            pJ4 = new Jugador(false, "COM3", 4);
+            pJ1 = new Persona(id, 1, Ludo);
+            pJ2 = new Computadora("@", 2, Ludo);
+            pJ3 = new Computadora("&", 3, Ludo);
+            pJ4 = new Computadora("#", 4, Ludo);
             break;
         case 2:
             cout << "Jugador 1 ingrese su identificador: "; cin >> id;
-            pJ1 = new Jugador(true, id, 1);
+            pJ1 = new Persona(id, 1, Ludo);
             cout << "Jugador 2 ingrese su identificador: "; cin >> id;
-            pJ2 = new Jugador(true, id, 2);
-            pJ3 = new Jugador(false, "COM1", 3);
-            pJ4 = new Jugador(false, "COM2", 4);
+            pJ2 = new Persona(id, 2, Ludo);
+            pJ3 = new Computadora("@", 3, Ludo);
+            pJ4 = new Computadora("&", 4, Ludo);
             break;
         case 3:
             cout << "Jugador 1 ingrese su identificador: "; cin >> id;
-            pJ1 = new Jugador(true, id, 1);
+            pJ1 = new Persona(id, 1, Ludo);
             cout << "Jugador 2 ingrese su identificador: "; cin >> id;
-            pJ2 = new Jugador(true, id, 2);
+            pJ2 = new Persona(id, 2, Ludo);
             cout << "Jugador 3 ingrese su identificador: "; cin >> id;
-            pJ3 = new Jugador(true, id, 3);
-            pJ4 = new Jugador(false, "COM1", 4);
+            pJ3 = new Persona(id, 3, Ludo);
+            pJ4 = new Computadora("@", 4, Ludo);
             break;
         case 4:
             cout << "Jugador 1 ingrese su identificador: "; cin >> id;
-            pJ1 = new Jugador(true, id, 1);
+            pJ1 = new Persona(id, 1, Ludo);
             cout << "Jugador 2 ingrese su identificador: "; cin >> id;
-            pJ2 = new Jugador(true, id, 2);
+            pJ2 = new Persona(id, 2, Ludo);
             cout << "Jugador 3 ingrese su identificador: "; cin >> id;
-            pJ3 = new Jugador(true, id, 3);
+            pJ3 = new Persona(id, 3, Ludo);
             cout << "Jugador 4 ingrese su identificador: "; cin >> id;
-            pJ4 = new Jugador(true, id, 4);
+            pJ4 = new Persona(id, 4, Ludo);
             break;
     }
 
-    cout << "Jugador 1: " << pJ1->get_id();
+    cout << "\nJugador 1: " << pJ1->get_id();
     cout << "\nJugador 2: " << pJ2->get_id();
     cout << "\nJugador 3: " << pJ3->get_id();
     cout << "\nJugador 4: " << pJ4->get_id();
 
+    Display(Ludo);
 
+    cout <<"\nJuguemos!\n";
+    pJ1->Turno(Ludo);
 
     return 0;
 }
